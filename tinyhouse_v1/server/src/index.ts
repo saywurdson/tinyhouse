c4ee475a55e05d6cb2126a83a30e6a8625bce08d
saywurdson <34385409+saywurdson@users.noreply.github.com>
2021-03-05 13:35:24 -0800

    tinyhouse start

---
+++ tinyhouse_v1/server/src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+const port = 9000;
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(port);
+console.log(`[app]: http://localhost:${port}`);
