5a75f39f51a18fce578994d1ad5824034afe08b6
saywurdson <34385409+saywurdson@users.noreply.github.com>
2021-03-05 21:35:50 -0800

    finished Apollo Server set up

--- tinyhouse_v1/server/src/index.ts
+++ tinyhouse_v1/server/src/index.ts
@@ -1,26 +1,14 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import {listings} from "./listings";
+import {ApolloServer} from 'apollo-server-express';
+import {typeDefs, resolvers} from './graphql';
 
 const app = express();
 const port = 9000;
 
+const server = new ApolloServer({typeDefs, resolvers});
+server.applyMiddleware({app, path: '/api'});
+
 app.use(express.json());
-
-app.get("/listings", (_req, res) => {
-  return res.send(listings);
-});
-
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-  return res.send("failed to delete listing");
-});
 
 app.listen(port);
 console.log(`[app]: http://localhost:${port}`);
